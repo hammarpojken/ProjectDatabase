877f19bae8d2eb44d06f6bd362ca3624d2af231f
Niclas Hammar <nicke_hammar@hotmail.com>
2017-06-08 23:36:32 +0200

    Added funktion to add new student to the database

--- Database/WindowsFormsApplication1/DatabaseHandler.cs
+++ Database/WindowsFormsApplication1/DatabaseHandler.cs
@@ -10,8 +10,8 @@ namespace WindowsFormsApplication1
 {
     class DatabaseHandler
     {
-        public DataSet ds = new DataSet();
-        public DataTable dt = new DataTable();
+        private DataSet ds = new DataSet();
+        private DataTable dt = new DataTable();
         public string connstring = "Server=127.0.0.1;Port=5432;Database=project;User Id=postgres;Password=backstab1870;";
         
         public DatabaseHandler()
@@ -33,10 +33,46 @@ namespace WindowsFormsApplication1
             da.Fill(ds);
             dt = ds.Tables[0];
             conn.Close();
+            
             return dt;
         }
-        public void updateDB ()
+        public void newStudentRow (DataTable dt)
         {
+            NpgsqlConnection conn = new NpgsqlConnection(connstring);
+            conn.Open();
+            NpgsqlCommand command = new NpgsqlCommand("INSERT INTO student VALUES (:personnr, :fname, :lname, :adress, :postnr, :ort, :pr,:ma,:sv,:en,:gy)", conn);
+
+            try
+            {
+                command.Parameters.Add(new NpgsqlParameter("personnr", NpgsqlTypes.NpgsqlDbType.Bigint));
+                command.Parameters.Add(new NpgsqlParameter("fname", NpgsqlTypes.NpgsqlDbType.Text));
+                command.Parameters.Add(new NpgsqlParameter("lname", NpgsqlTypes.NpgsqlDbType.Text));
+                command.Parameters.Add(new NpgsqlParameter("adress", NpgsqlTypes.NpgsqlDbType.Text));
+                command.Parameters.Add(new NpgsqlParameter("postnr", NpgsqlTypes.NpgsqlDbType.Integer));
+                command.Parameters.Add(new NpgsqlParameter("ort", NpgsqlTypes.NpgsqlDbType.Text));
+                command.Parameters.Add(new NpgsqlParameter("pr", NpgsqlTypes.NpgsqlDbType.Char));
+                command.Parameters.Add(new NpgsqlParameter("ma", NpgsqlTypes.NpgsqlDbType.Char));
+                command.Parameters.Add(new NpgsqlParameter("sv", NpgsqlTypes.NpgsqlDbType.Char));
+                command.Parameters.Add(new NpgsqlParameter("en", NpgsqlTypes.NpgsqlDbType.Char));
+                command.Parameters.Add(new NpgsqlParameter("gy", NpgsqlTypes.NpgsqlDbType.Boolean));
+                command.Parameters[0].Value = dt.Rows[0][0];
+                command.Parameters[1].Value = dt.Rows[0][1];
+                command.Parameters[2].Value = dt.Rows[0][2];
+                command.Parameters[3].Value = dt.Rows[0][3];
+                command.Parameters[4].Value = dt.Rows[0][4];
+                command.Parameters[5].Value = dt.Rows[0][5];
+                command.Parameters[6].Value = dt.Rows[0][6];
+                command.Parameters[7].Value = dt.Rows[0][7];
+                command.Parameters[8].Value = dt.Rows[0][8];
+                command.Parameters[9].Value = dt.Rows[0][9];
+                command.Parameters[10].Value = dt.Rows[0][10];
+                command.ExecuteNonQuery();
+                conn.Close(); 
+            } catch (NpgsqlException e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Message);
+            }
+           
 
         }
 
--- Database/WindowsFormsApplication1/StudentPopup.cs
+++ Database/WindowsFormsApplication1/StudentPopup.cs
@@ -13,7 +13,7 @@ namespace WindowsFormsApplication1
     public partial class StudentPopup : Form
     {
         private DatabaseHandler dbhandler = new DatabaseHandler();
-        private DataTable dt = new DataTable();
+        private DataTable dtstudent = new DataTable();
         private DataTable dtbetyg = new DataTable();
         private string studentid;
         
@@ -25,23 +25,24 @@ namespace WindowsFormsApplication1
         // laddar in all data om studenten
         private void Form2_Load(object sender, EventArgs e)
         {
-            dt = dbhandler.selectDB("SELECT * FROM student WHERE personnr = " + studentid);
+            dtstudent = dbhandler.selectDB("SELECT * FROM student WHERE personnr = " + studentid).Copy();
            
-            this.Text = dt.Rows[0].Field<string>(1) + " " + dt.Rows[0].Field<string>(2);
-            textBox1.Text = dt.Rows[0].Field<string>(1);
-            textBox2.Text = dt.Rows[0].Field<string>(2);
-            textBox3.Text = dt.Rows[0].Field<Int64>(0).ToString();
-            textBox4.Text = dt.Rows[0].Field<string>(3);
-            textBox5.Text = dt.Rows[0].Field<string>(5);
-            textBox6.Text = dt.Rows[0].Field<int>(4).ToString();
-            textBox7.Text = dt.Rows[0].Field<string>(6).ToString();
-            textBox8.Text = dt.Rows[0].Field<string>(7).ToString();
-            textBox9.Text = dt.Rows[0].Field<string>(8).ToString();
-            textBox10.Text = dt.Rows[0].Field<string>(9).ToString();
-            if (dt.Rows[0].IsNull(10)) 
+            this.Text = dtstudent.Rows[0].Field<string>(1) + " " + dtstudent.Rows[0].Field<string>(2);
+            textBox1.Text = dtstudent.Rows[0][1].ToString();
+            textBox2.Text = dtstudent.Rows[0].Field<string>(2);
+            textBox3.Text = dtstudent.Rows[0].Field<Int64>(0).ToString();
+            textBox4.Text = dtstudent.Rows[0].Field<string>(3);
+            textBox5.Text = dtstudent.Rows[0].Field<string>(5);
+            textBox6.Text = dtstudent.Rows[0].Field<int>(4).ToString();
+            textBox7.Text = dtstudent.Rows[0].Field<string>(6).ToString();
+            textBox8.Text = dtstudent.Rows[0].Field<string>(7).ToString();
+            textBox9.Text = dtstudent.Rows[0].Field<string>(8).ToString();
+            textBox10.Text = dtstudent.Rows[0].Field<string>(9).ToString();
+            
+            if (dtstudent.Rows[0].IsNull(10)) 
             {
                 textBox11.Text = "Inget angivet";
-            } else if (dt.Rows[0].Field<Boolean>(10).ToString().Equals("False")) {
+            } else if (dtstudent.Rows[0].Field<Boolean>(10).ToString().Equals("False")) {
                 textBox11.Text = "Nej";
             } else
             {
@@ -50,8 +51,25 @@ namespace WindowsFormsApplication1
             }
             dtbetyg = dbhandler.selectDB("SELECT kurs.namn, betyg.betyg FROM kurs JOIN betyg ON kurs.kursid = betyg.kurs WHERE betyg.student =" + studentid);
             this.dataGridView1.DataSource = dtbetyg;
+            
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dtstudent.Rows[0][0] = Int64.Parse(textBox3.Text);
+            dtstudent.Rows[0][1] = textBox1.Text.ToString();
+            dtstudent.Rows[0]["lname"] = textBox2.Text.ToString();
+            dtstudent.Rows[0]["adress"] = textBox4.Text.ToString();
+            dtstudent.Rows[0]["postnr"] = Int32.Parse(textBox6.Text);
+            dtstudent.Rows[0]["ort"] = textBox5.Text.ToString();
+            dtstudent.Rows[0]["pr"] = textBox7.Text.ToString();
+            dtstudent.Rows[0]["ma"] = textBox8.Text.ToString();
+            dtstudent.Rows[0]["sv"] = textBox9.Text.ToString();
+            dtstudent.Rows[0]["en"] = textBox10.Text.ToString();
+            dtstudent.Rows[0]["gy"] = Boolean.Parse(textBox11.Text);
 
+            dbhandler.newStudentRow(dtstudent);
+
+        }
     }
 }
